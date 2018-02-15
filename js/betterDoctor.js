f5c3ae991546f5d5e56dfae943f4e67631b0f142
Guest User <Guest@Epicodus-3A.local>
2018-02-15 15:41:50 -0800

    refractor function

---
+++ js/betterDoctor.js
@@ -0,0 +1,21 @@
+import {apiKey} from './../.env';
+
+export function callBetterDoctor() {
+  let url = `https://api.betterdoctor.com/2016-03-01/doctors?location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${apiKey}`;
+  let result = "noChange";
+  $.ajax({
+    url: url,
+    type: 'GET',
+    data: {
+      format: 'json'
+    },
+    success: function(response) {
+      for(let i = 0; i < response.data.length ; i ++){
+        $('#result').append(`<div >${response.data[i].practices[0].location_slug}</div>`);
+      }
+    },
+    error: function() {
+      $('#errors').text("There was an error processing your request. Please try again.");
+      }
+  });
+}
